ba62ccdca31eea180c3a389f659fca6fcbd1bb11
Vincent Xu <xwc070902@gmail.com>
2023-06-11 14:15:22 -0700

    ac.py

---
+++ actracker.py
@@ -0,0 +1,10 @@
+class activity:
+    def __init__(self,user,age,date,hours,what_did,feeling):
+        self.user=user
+        self.age=age
+        self.date=date
+        self.hours=hours
+        self.what_did=what_did
+        self.feeling=feeling
+    def analyze_activity(self):
+        

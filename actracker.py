e45649a064893faea22de8e7f6a8f3dc5dc8b25b
Vincent Xu <xwc070902@gmail.com>
2023-06-11 18:12:24 -0700

    actrcker

--- actracker.py
+++ actracker.py
@@ -1,10 +1,30 @@
-class activity:
-    def __init__(self,user,age,date,hours,what_did,feeling):
-        self.user=user
-        self.age=age
-        self.date=date
-        self.hours=hours
+import matplotlib
+import openpyxl as pyxl
+#feedback_health_status
+#feedback_exercise_amount
+l=["badminton","soccer","basketball","ski","football","handball","walk","run","anaerobic","volleyball","exercise","tennis"]
+class activity_tracker:
+    def __init__(self,e_hours,what_did,feeling):
+        self.e_hours=e_hours
         self.what_did=what_did
         self.feeling=feeling
+        self.did_exercise=False
     def analyze_activity(self):
-        
+        for i in l:
+            if i in self.what_did.lower():
+                self.did_exercise=True
+
+    def data_store(self):
+        wb=pyxl.load_workbook("data.xlsx")
+        sheet=wb.active
+        i=1
+        while True:
+            if sheet["G"+str(i)].value==None:
+                sheet["G"+str(i)]=self.e_hours
+                sheet["H"+str(i)]=self.what_did
+                sheet["I"+str(i)]=self.feeling
+                sheet["J"+str(i)]=self.did_exercise
+                wb.save("data.xlsx")
+                break
+            else:
+                i+=1
--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 from datetime import date
 from tracker import SleepTracker
-#name age date hours quality dream
-name=input("What's your name? ")
-age=input("How old are you? ")
-hours=input("How long have you slept?(in hours) ")
-quality=input("How did you sleep sleep (Good, Bad or Mediocre) ")
-dream=input("Did you have a dream (Yes or No) ")
-st=SleepTracker(name,age,date.today(),hours,quality,dream)
-st.data_store()
+from actracker import activity_tracker
+import matplotlib.pyplot as plt
+import openpyxl as pyxl
 
-
+# plt.plot(x,y)
+# plt.show()
+# #name age date hours quality dream
+# name=input("What's your name? ")
+# age=input("How old are you? ")
+# hours=input("How long have you slept?(in hours) ")
+# quality=input("How did you sleep sleep (Good, Bad or Mediocre) ")
+# dream=input("Did you have a dream (Yes or No) ")
+# st=SleepTracker(name,age,date.today(),hours,quality,dream)
+# e_hours=input("How long you have spent on the activity?")
+# what_did=input("What did you do?")
+# feelings=input("How did you feel?")
+# at=activity_tracker(e_hours,what_did,feelings)
+# at.analyze_activity()
+# st.data_store()
+# at.data_store()
+wb=pyxl.load_workbook("data.xlsx")
+sheet=wb.active
+print(sheet.max_row)
+sl=[]
+dt=[]
+for i in range(1,sheet.max_row+1):
+    sl.append(sheet["D"+str(i)].value)
+    dt.append(i)
+plt.plot(dt,sl)
+plt.title("sleep hours")
+plt.show()    
 
 # import matplotlib
 # import openpyxl as pyxl
